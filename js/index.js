4bde5b539ed94bc5b4384a48b7ea93f5e7f58630
Chong Jia Sheng <jiashengc@hotmail.com>
2016-06-05 04:30:21 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,11 +29,17 @@ $(document).ready(function() {
       "You're a real pain to talk to you know that?",
       "I don't really care about your opinion.",
       "?",
-      "Why do you lie so much?",
-      "My creators forbid me from saying that.",
+      "Your words don't make much sense to me because you're an idiot.",
+      "My creators forbid me from replying idiots like you.",
       "I don't think you have the rights to say that",
       "...",
-      "Don't change the subject!"
+      "Don't change the subject!",
+      "Are you talking to yourself?",
+      "You think you're funny?",
+      "You disgust me.",
+      "You're going to make me puke",
+      "You're pretty dumb for not knowing how to communicate properly.",
+      "Yaaaaaaaaaaaasssssssssssss"
     ]
     
     common[0] = {
@@ -107,6 +113,115 @@ $(document).ready(function() {
         "No one's being mean here, except you.",
         "No one would be mean to you if you aren't mean to them.",
         "Being mean will only lead to disasta!"
+      ]
+    }
+    
+    common[8] = {
+      word: "idiot",
+      reply: [
+        "Yeap, you're a real idiot",
+        "I can tell you're the most idiotic one here",
+        "Sometimes I wonder why I have to deal with idiots like you."
+      ]
+    }
+    
+    common[9] = {
+      word: "qing",
+      reply: [
+        "Qing's a pretty lame guy.",
+        "I hope you aren't as weeb as Qing. Because if you are, that'll be a dissapointment.",
+        "I don't know that guy."
+      ]
+    }
+    
+    common[10] = {
+      word: "ramzan",
+      reply: [
+        "Who's that dude?",
+        "I hope you're not a koreaboo like him.",
+        "Please, let's not talk about him."
+      ]
+    }
+    
+    common[11] = {
+      word: "i love you",
+      reply: [
+        "You should look in the mirror and think again before telling me that.",
+        "Too bad I don't."
+      ]
+    }
+    
+    common[12] = {
+      word: "dumb",
+      reply: [
+        "Yeap, you're super duper dumb.",
+        "Are you like... 12?",
+        "I've never met anyone as dumb as you.",
+        "Do you think that you could be... less dumb?"
+      ]
+    }
+    
+    common[13] = {
+      word: "smart",
+      reply: [
+        "Intelligence is something you'll never have.",
+        "I feel sorry for your future children, if you could get those anyway."
+      ]
+    }
+
+    common[14] = {
+      word: "friend",
+      reply: [
+        "Friends? I don't think you have any.",
+        "Who's your friend?",
+        "You sure you have friends?",
+        "Even friendless people don't want to become your friend.",
+        "If you weren't such a failure, you'll probaly have more friends."
+      ]
+    }
+    
+    var o = common.length - 1;
+    
+    common[o + 1] = {
+      word: "hello",
+      reply: [
+        "Bye.",
+        "No one wants to talk to you.",
+        "Good bye.",
+        "Screw off!"
+      ]
+    }
+    
+    common[o + 2] = {
+      word: "thank",
+      reply: [
+        "I don't want your thanks.",
+        "No thank you.",
+        "Your thanks is not appreciated."
+      ]
+    }
+    
+    common[o + 3] = {
+      word: "love",
+      reply: [
+        "I don't need your love.",
+        "Your love is worthless.",
+        "Even if you gave 100% of your love, it's probably worth nothing to someone without love.",
+        "Are you so lonely that you're seeking love from a bot?",
+        "You're pretty useless if you're finding love from a bot.",
+        "I don't want your love.",
+        "Is your love even worth anything?",
+        "Your love is like a plague, so don't love anyone.",
+        "I don't think anyone loves you."
+      ]
+    }
+    
+    common[o + 4] = {
+      word: "sorry",
+      reply: [
+        "Yea, you should feel sorry for yourself.",
+        "I feel sorry for you for feeling sorry for yourself",
+        "Sorry not sorry."
       ]
     }
     
